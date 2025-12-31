0c9949864e76f6d4cad337c8b0cbf5c36d1dcddb
Akhila-Thada <akhila.thada@gmail.com>
2025-12-31 15:59:27 +0530

    Updated socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,26 @@
 import { io } from "socket.io-client";
 
-console.log("Connecting to:", import.meta.env.VITE_API_URL);
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL;
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:5001", {
+console.log("Connecting socket to:", SOCKET_URL);
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"], 
+  withCredentials: true,
   autoConnect: true,
 });
 
-socket.on("connect", () => console.log("Socket connected (client):", socket.id));
-console.log("Dashboard trying socket");
-socket.on("connect", () => console.log("Dashboard connected"));
-socket.on("connect_error", err => console.log(err.message));
+socket.on("connect", () =>
+  console.log(" Socket connected:", socket.id)
+);
 
-socket.on("disconnect", () => console.log("Socket disconnected (client)"));
+socket.on("connect_error", (err) =>
+  console.error("Socket error:", err.message)
+);
+
+socket.on("disconnect", () =>
+  console.log(" Socket disconnected")
+);
 
 export default socket;
